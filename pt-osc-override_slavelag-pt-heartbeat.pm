3f9f57db9710338310297ca255000947c74d92d5
Kenny Gryp <gryp@dakin.be>
2014-03-25 13:59:37 +0100

    - check if replication is stopped and return the appropriate message - $oktorun must be updated to the reference variable $oktorun. - formatting optimizations

--- pt-osc-override_slavelag-pt-heartbeat.pm
+++ pt-osc-override_slavelag-pt-heartbeat.pm
@@ -13,24 +13,49 @@ sub new {
 
 sub override_slavelag_check {
    my ($self, %args) = @_;
+   # oktorun is a reference, also update it using $$oktorun=0;
+   my $oktorun=$args{oktorun};
+
    print "PLUGIN override_slavelag_check: pt-heartbeat will be checked\n";
 
-   return sub {
+   my $get_lag = sub {
          my ($cxn) = @_;
          my $dbh = $cxn->dbh();
+
          if ( !$dbh || !$dbh->ping() ) {
             eval { $dbh = $cxn->connect() };  # connect or die trying
             if ( $EVAL_ERROR ) {
                chomp $EVAL_ERROR;
+               $$oktorun=0;
                die "Lost connection to replica " . $cxn->name()
                   . " while attempting to get its lag ($EVAL_ERROR)\n";
             }
          }
-         my $sth = $dbh->prepare("SELECT min(unix_timestamp(now()) - unix_timestamp(concat(substr(ts,1,10), ' ', substr(ts,12,8)))) as lag_sec FROM percona.heartbeat");
-         $sth->execute();
-         my $res =  $sth->fetchrow_hashref();
+
+         # If replication is not running, then it should be properly reported
+
+         my $slavestatus = $dbh->selectrow_hashref("SHOW SLAVE STATUS");
+         if (     ($slavestatus->{slave_io_running} ne "Yes")
+             ||   ($slavestatus->{slave_sql_running} ne "Yes") ) {
+            return;
+         }
+
+         my $res = $dbh->selectrow_hashref(
+            "SELECT min(unix_timestamp(now())
+                        - unix_timestamp(
+                           concat(substr(ts,1,10),
+                                 ' ', 
+                                 substr(ts,12,8)
+                                 )
+                           )
+                     ) as lag_sec 
+            FROM percona.heartbeat");
+
+         # we return oktorun and the lag
          return $res->{lag_sec};
-      };
+   };
+
+   return $get_lag;
 }
 
 1;
